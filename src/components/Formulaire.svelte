72e619585dadd23bb2a12e751757ee4ac7c8a1af
Github1234fab <fabien.marceau@gmail.com>
2024-07-17 13:13:15 +0200

    erfzfzrf

--- src/components/Formulaire.svelte
+++ src/components/Formulaire.svelte
@@ -1,4 +1,4 @@
-<section class="formulaire">
+<section class="formulaire" id="Contact">
 
 <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
 <input type="hidden" name="form-name" value="contact">
