f302dd1e57e3569db97832aeb8dd1c2e5f0d3c43
Github1234fab <fabien.marceau@gmail.com>
2025-03-24 10:21:44 +0100

    ok

---
+++ src/components/TestForm.svelte
@@ -0,0 +1,29 @@
+<script>
+    let Nom = "";
+    let Email = "";
+    let Demande = "";
+    let Téléphone = "";
+
+    const sheetUrl = "https://script.google.com/macros/s/AKfycbx3gxGxfJM6k1bzzskfHYa0l6crpxgRc1Xfy2lxGqtnGOPU0bGa3TwJukUcDZjw9hLVCA/exec"; 
+  
+    async function submitForm() {
+      const response = await fetch(sheetUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ Nom, Email, Demande, Téléphone }),
+      });
+  
+      const text = await response.text();
+      console.log("Réponse du serveur :", text);
+    }
+  </script>
+  
+  <form on:submit|preventDefault={submitForm}>
+    <input type="text" bind:value={Nom} placeholder="Nom" required />
+    <input type="email" bind:value={Email} placeholder="Email" required />
+    <input type="text" bind:value={Demande} placeholder="Demande" required />
+    <input type="tel" bind:value={Téléphone} placeholder="Téléphone" required />
+
+
+    <button type="submit">Envoyer</button>
+  </form>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script>
-        import Hero from "../components/Hero.svelte";
-        import About from "../components/About.svelte";
-        import Products from "../components/Products.svelte";
-        import Service from "../components/Service.svelte";
-        import Social from "../components/Social.svelte";
-        import CTA from "../components/CTA.svelte";
-        import Formulaire from "../components/Formulaire.svelte";
-        import Resa from "../components/Resa.svelte";
+  import Hero from "../components/Hero.svelte";
+  import About from "../components/About.svelte";
+  import Products from "../components/Products.svelte";
+  import Service from "../components/Service.svelte";
+  import Social from "../components/Social.svelte";
+  import CTA from "../components/CTA.svelte";
+  import Formulaire from "../components/Formulaire.svelte";
+  import Resa from "../components/Resa.svelte";
+  import TestForm from "../components/TestForm.svelte";
 </script>
 
 <Hero />
@@ -15,5 +16,5 @@
 <Service />
 <Social />
 <Resa />
-<!-- <CTA /> -->
+<TestForm/>
 <Formulaire />
